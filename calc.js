80d8ab674a9be1663cfe8d8d3fdfa2d88c3eb173
Willian VInicius Ramalho <49045509+willianramalho@users.noreply.github.com>
2023-02-28 15:19:49 -0300

    Add files via upload

---
+++ calc.js
@@ -0,0 +1,20 @@
+const teclaZero = document.getElementById('tecla_0');
+const teclaUm = document.getElementById('tecla_1');
+const teclaDois = document.getElementById('tecla_2');
+const teclaTres = document.getElementById('tecla_3');
+const teclaQuatro = document.getElementById('tecla_4');
+const teclaCinco = document.getElementById('tecla_5');
+const teclaSeis = document.getElementById('tecla_6');
+const teclaSete = document.getElementById('tecla_7');
+const teclaOito = document.getElementById('tecla_8');
+const teclaNove = document.getElementById('tecla_9');
+
+const teclaPonto = document.getElementById('tecla_ponto');
+const teclaIgual = document.getElementById('tecla_igual');
+const teclaSoma = document.getElementById('tecla_soma');
+const teclaDivide = document.getElementById('tecla_divide');
+const teclaMultiplica = document.getElementById('tecla_multiplica');
+const teclaSubtrai = document.getElementById('tecla_subtrai');
+
+teclaZero.value = 0;
+console.log(teclaZero)
